7f5f9d474d54047782abf74bf624915e849c1ac0
Giovanni <giovanni.ricotta@ynov.com>
2025-03-09 14:54:49 +0100

    updateCreate600: Fixed bug texture

--- src/main/java/net/nuclearteam/createnuclear/content/multiblock/reinforced/ReinforcedGlassBlock.java
+++ src/main/java/net/nuclearteam/createnuclear/content/multiblock/reinforced/ReinforcedGlassBlock.java
@@ -1,13 +1,20 @@
 package net.nuclearteam.createnuclear.content.multiblock.reinforced;
 
+import com.simibubi.create.content.decoration.palettes.ConnectedGlassBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.GlassBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class ReinforcedGlassBlock extends GlassBlock {
+public class ReinforcedGlassBlock extends ConnectedGlassBlock {
     public ReinforcedGlassBlock(Properties properties) {
         super(properties);
     }
 
-    public static final DirectionProperty FACING = BlockStateProperties.FACING;
 }
